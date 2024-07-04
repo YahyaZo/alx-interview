534725725628ac8ba5e5b46c9a54fbd38d757d02
YahyaZo <khayoussef.yahya20@gmail.com>
2024-07-04 12:52:29 +0100

    pycodestyle issue

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,23 +3,25 @@
 This module provides a function to determine if all boxes can be unlocked.
 """
 
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
-    
+
     Args:
-        boxes (list of lists): A list where each element is a list of keys for other boxes.
-    
+        boxes (list of lists): A list where each element is a list of keys
+        for other boxes.
+
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
     if not boxes:
         return False
-    
+
     n = len(boxes)
     opened = [False] * n
     opened[0] = True  # The first box is always open
-    
+
     queue = [0]  # Start with the first box
 
     while queue:
