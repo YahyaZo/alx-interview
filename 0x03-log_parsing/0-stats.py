576ebee6cc685d62623c2e1056ab00014c794870
Yahya <115636036+YahyaZo@users.noreply.github.com>
2024-07-22 00:22:55 +0100

    Update 0-stats.py
    pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,74 +1,54 @@
 #!/usr/bin/python3
-"""
-Module for log parsing script.
-Reads from stdin and computes metrics.
-"""
-
 
 import sys
-import signal
-import re
 
 
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
-# Initialize counters and accumulators
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
 total_file_size = 0
-status_code_counts = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-line_count = 0
-
-
-# Regular expression to match the log line format
-log_pattern = re.compile(
-    r'^\d+\.\d+\.\d+\.\d+ - \[.+\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$')
-
-
-
-
-def print_stats():
-    """Prints the accumulated statistics."""
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_code_counts.keys()):
-        if status_code_counts[code] > 0:
-            print(f"{code}: {status_code_counts[code]}")
-
-
-
-def signal_handler(sig, frame):
-    """Handles the keyboard interruption signal."""
-    print_stats()
-    sys.exit(0)
-
-
-
-# Set up the signal handler for keyboard interruption
-signal.signal(signal.SIGINT, signal_handler)
-
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        match = log_pattern.match(line)
-        if match:
-            status_code = match.group(1)
-            file_size = int(match.group(2))
-            total_file_size += file_size
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-            line_count += 1
-        if line_count == 10:
-            print_stats()
-            line_count = 0
-except Exception as e:
-    sys.stderr.write(f"Error: {str(e)}\n")
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
+        if len(parsed_line) > 2:
+            counter += 1
 
-# Print remaining statistics at the end
-print_stats()
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
